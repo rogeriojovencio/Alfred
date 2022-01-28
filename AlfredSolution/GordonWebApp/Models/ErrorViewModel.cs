a0b25b77baf79b34d09535b758fba92d6684be46
Rogerio Jovencio <r.jovencio@avanade.com>
2022-01-28 12:17:45 -0300

    Criacao WebApp

---
+++ AlfredSolution/GordonWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GordonWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
