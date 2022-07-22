81523e8880131a3524dcc98b83b964ac9b811328
Rogerio Jovencio <r.jovencio@avanade.com>
2022-07-22 11:56:47 -0300

    Criando os metodos de Aceeso Seleniumn

--- AlfredSolution/AlfredCmd/Program.cs
+++ AlfredSolution/AlfredCmd/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Office.Interop.Excel;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Excel = Microsoft.Office.Interop.Excel;
 using sl = GordonSelenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GordonSelenium;
+using System.Threading;
+
 namespace AlfredCmd
 {
     class Program
@@ -15,12 +21,61 @@ namespace AlfredCmd
 
             //-----Testar o codigo dos Projetos
 
-            //Entra na pagina do Cliente.
-            int ret = sl.MainBrowsers.OpenBrowser("https://www.google.com");
-
+            // abre o browser e 
+            //sl.MainBrowsers.OpenBrowser("https://www.google.com");
+            FirstScriptTest p = new FirstScriptTest();
+            p.ChromeSession();
 
         }
-            
     }
+    [TestClass]
+    public class FirstScriptTest
+    {
+        private bool ret = false;
+
+        [TestMethod]
+        public void ChromeSession()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://google.com");
+
+            var title = driver.Title;
+            Assert.AreEqual("Google", title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var searchBox = driver.FindElement(By.Name("q"));
+            var searchButton = driver.FindElement(By.Name("btnK"));
+
+            searchBox.SendKeys("Selenium");
+            searchButton.Click();
+
+            searchBox = driver.FindElement(By.Name("q"));
+            var value = searchBox.GetAttribute("value");
+
+            //=============Teste para a Nova classe===================================
+            MainBrowsers obj = new MainBrowsers();
+
+            //Abre a pagina. 
+            //Localiza se a pagina esta no ar
+            //Localiza o campo se existe
+
+            Thread.Sleep(1000);
+            //Testando se o objeto existe na tela.
+            bool re = obj.SearchTextField(driver, "q", MainBrowsers.SType.Name  );
+
+            // criar um enumerable para definir o tipo de pesquisa utilizando apenas uma função.
+
+
+            //==========================================================
+            Assert.AreEqual("Selenium2", value);
+
+            driver.Quit();
+        }
+
+    }
+
+
 }
 
--- AlfredSolution/GordonSelenium/MainBrowsers.cs
+++ AlfredSolution/GordonSelenium/MainBrowsers.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,25 @@ using System.Threading.Tasks;
 
 namespace GordonSelenium
 {
+    
     public class MainBrowsers
     {
-        public static int OpenBrowser(string url)
+        private static ChromeDriver driver;
+        private bool ret = false;
+
+        // Criar um enumerable para definir o tipo de pesquisa utilizando apenas uma função.
+        #region SType
+        public enum SType
+        {
+           Id = 1,
+           Name = 2,
+           Class = 3                
+        }
+        #endregion SType
+
+        //Open o Browser
+        #region OpenBrowser
+        public static ChromeDriver OpenBrowser(string url)
         {
             try
             {
@@ -18,6 +35,25 @@ namespace GordonSelenium
                 var driver = new ChromeDriver();
                 driver.Navigate().GoToUrl(url);
                 Console.WriteLine($"Url Aberto ok:{url}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                Console.ReadKey();
+
+            }
+            return driver;
+        }
+        #endregion  OpenBrowser
+
+        //Fecha o Browser
+        #region CloseBrowser
+        public static int CloseBrowser(ChromeDriver driver)
+        {
+            try
+            {
+                driver.Quit();
                 return 1;
             }
             catch (Exception ex)
@@ -25,9 +61,97 @@ namespace GordonSelenium
 
                 Console.WriteLine(ex);
                 Console.ReadKey();
-                return 0;                
+                return 0;
             }
         }
+        #endregion CloseBrowser
+
+        //Localiza o elemento na Tela
+        #region SearchTextField       
+        public bool SearchTextField(ChromeDriver drv, string textfield, SType t)
+        {
+            ret = false;
+            object ElementId;
+            try
+            {
+                switch (t)
+                {
+                    case SType.Id:
+                        ElementId = drv.FindElement(By.Id(textfield));
+                        break;
+                    case SType.Name:
+                        ElementId = drv.FindElement(By.Name(textfield));
+                        break;
+                    case SType.Class:
+                        ElementId = drv.FindElement(By.ClassName(textfield));
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                ret = false;
+            }
+            return ret;
+        }
+        #endregion SearchTextField
+
+        //Clicar no elemento na Tela
+        #region SearchTextFieldClick        
+        public bool SearchTextFieldClick(ChromeDriver drv, string textfield, SType t)
+        {
+            ret = false;
+            object ElementId;
+            try
+            {
+                switch (t)
+                {
+                    case SType.Id:
+                        drv.FindElement(By.Id(textfield)).Click();
+                        break;
+                    case SType.Name:
+                        drv.FindElement(By.Name(textfield)).Click();
+                        break;
+                    case SType.Class:
+                        drv.FindElement(By.ClassName(textfield)).Click();
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                ret = false;
+            }
+            return ret;
+        }
+        #endregion SearchTextFieldClick
+
+        //Escreve no Elemento Selecionado
+        #region SearchTextFieldWrite       
+        public bool SearchTextFieldWrite(ChromeDriver drv, string textfield, SType t)
+        {
+            ret = false;
+            object ElementId;
+            try
+            {
+                switch (t)
+                {
+                    case SType.Id:
+                        ElementId = drv.FindElement(By.Id(textfield));
+                        break;
+                    case SType.Name:
+                        ElementId = drv.FindElement(By.Name(textfield));
+                        break;
+                    case SType.Class:
+                        ElementId = drv.FindElement(By.ClassName(textfield));
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                ret = false;
+            }
+            return ret;
+        }
+        #endregion SearchTextFieldWrite
 
     }
 }
